1d88c62a4c9433d4f885a6e2860f28d7fcbf5a03
Grace Yu <grace.yu4444@gmail.com>
2014-10-06 23:45:15 -0600

    methods_and_flow_control_Oct6th2014JN_HW

---
+++ methods_and_flow_control/string_practice.rb
@@ -0,0 +1,13 @@
+#!usr/bin/env ruby
+
+variable = 'this is a string to practice with'
+
+puts "#{variable}"
+puts "#{variable}".downcase
+puts "#{variable}".capitalize
+puts "#{variable}".upcase
+puts "#{variable}".capitalize.gsub('string', "'string'")
+puts "The string '#{variable}' has #{variable.length} characters"
+puts "#{variable.reverse}"
+puts "#{(variable[20..28] * 3).strip}!"
+
